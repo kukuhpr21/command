089ee800d73e40012c04aa0d8586a0fbbce341c7
Kukuh182 <kukuhpr21@gmail.com>
2024-09-25 23:56:04 +0700

    4. livewire action

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Livewire\About;
+use App\Livewire\Contact;
+use App\Livewire\Home;
+use \App\Livewire\Posts\Index as PostIndex;
+use App\Livewire\Users\Show as UsersShow;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Home::class)->name('home');
+Route::get('/about', About::class)->name('about');
+Route::get('/contact', Contact::class)->name('contact');
+Route::get('/posts', PostIndex::class)->name('posts');
+Route::get('/users/{user}', UsersShow::class)->name('users.show');
